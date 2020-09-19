9dcfbb25f1fde13ca490539cf8f8ee52fccfd1c2
Diana Rakitina <69615229+drakitina@users.noreply.github.com>
2020-09-19 12:59:08 -0400

    practice calculation

---
+++ calculation.py
@@ -0,0 +1,21 @@
+#print(5*5*5)
+
+def a_plus_b(a,b):
+    print(f'sum of {a} and {b} equals {a + b}')
+
+a_plus_b(100,200)
+
+def a_minus_b(a,b):
+    print(f'difference of {a} and {b} equals {a - b}')
+
+a_minus_b(100, 200)
+
+def a_divide_b(a,b):
+    print(f'quotient of {a} and {b} equals {a / b}')
+
+a_divide_b(100, 200)
+
+def a_multiply_b(a,b):
+    print(f'product of {a} and {b} equals {a * b}')
+
+a_multiply_b(100, 200)
