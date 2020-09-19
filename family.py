a91cbefe3b7230ba26f609d149bafc75a778407b
Diana Rakitina <69615229+drakitina@users.noreply.github.com>
2020-09-19 12:24:13 -0400

    Added family information script

---
+++ family.py
@@ -0,0 +1,37 @@
+# Print #1
+# print('Hello Diana')
+
+def my_name_and_age(name, age, occupation, hobby):
+    print(f'Hi, my name is {name}, I am {age} years old. I am {occupation}.')
+    print(f'My hobby is {hobby}\n')
+
+# Print #2
+name = "Diana"
+age = 11
+occupation = 'student'
+hobby = 'dance'
+my_name_and_age(name, age, occupation, hobby)
+
+name = "Zinira"
+age = 35
+occupation = 'shop woker and kitchen aid'
+hobby = 'beading and clay work'
+my_name_and_age(name, age, occupation, hobby)
+
+name = "Maksim"
+age = 35
+occupation = 'scientist'
+hobby = 'beer drinking and construction working'
+my_name_and_age(name, age, occupation, hobby)
+
+name = "Daniel"
+age = 3
+occupation = 'phone player'
+hobby = 'scream and messy around'
+my_name_and_age(name, age, occupation, hobby)
+
+name = "Dominic"
+age = 0.5
+occupation = 'sleeper'
+hobby = 'drink milk and drink baby formula'
+my_name_and_age(name, age, occupation, hobby)
